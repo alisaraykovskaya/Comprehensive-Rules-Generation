ab16fff8327c88752ac7f5ebdc992a62d2db1e10
alisaraykovskaya <Alisaraykovskaya@icloud.com>
2023-09-20 16:09:58 +0300

    binarizer bugfix

--- binarizer.py
+++ binarizer.py
@@ -210,11 +210,14 @@ def binarize(df, column_name, unique_threshold, q, exceptions_threshold, numeric
 
                 elif numerical_binarization=='range':
                     one_hot_cols = list(one_hot.columns)
+                    print(one_hot_cols)
                     one_hot_cols[0]=(float('-inf'), one_hot.columns[0].right)
                     if not type(one_hot_cols[-1])==str and not type(one_hot_cols[-1])==float:
                         one_hot_cols[-1]=(one_hot.columns[-1].left, float('inf'))
-                    else:
+                    elif not type(one_hot_cols[-2])==str and not type(one_hot_cols[-2])==float:
                         one_hot_cols[-2]=(one_hot.columns[-2].left, float('inf'))
+                    else:
+                        one_hot_cols[-3]=(one_hot.columns[-3].left, float('inf'))
                     one_hot.columns = one_hot_cols
                     dict_one_hot_values[column_name] = list(map(interval_bounds, list(one_hot.columns)))
 
